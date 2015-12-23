474f95f4f32fb170c305befaaf4c51bc20a06b33
JekyllJi <triones_2005@hotmail.com>
2015-12-23 16:18:04 +0800

    add: init NorthwindService project

---
+++ NorthwindService/App_Start/WebApiConfig.cs
@@ -0,0 +1,44 @@
+﻿using NorthwindService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace NorthwindService
+{
+  public static class WebApiConfig
+  {
+    //public static void Register(HttpConfiguration config)
+    //{
+    //  // Web API 配置和服务
+
+    //  // Web API 路由
+    //  config.MapHttpAttributeRoutes();
+
+    //  config.Routes.MapHttpRoute(
+    //      name: "DefaultApi",
+    //      routeTemplate: "api/{controller}/{id}",
+    //      defaults: new { id = RouteParameter.Optional }
+    //  );
+    //}
+
+    public static void Register(HttpConfiguration config)
+    {
+      var cors = new EnableCorsAttribute("*", "accept,content-type,origin,x-my-header", "get");
+      config.EnableCors(cors);
+
+      // New code:
+      ODataModelBuilder builder = new ODataConventionModelBuilder();
+      builder.EntitySet<Product>("Products");
+      builder.EntitySet<Category>("Categories");
+      
+      config.MapODataServiceRoute(
+          routeName: "ODataRoute",
+          routePrefix: null,
+          model: builder.GetEdmModel());
+    }
+  }
+}
---
+++ NorthwindService/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using NorthwindService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+
+namespace NorthwindService.Controllers
+{
+  public class ProductsController : ODataController
+  {
+    NothwindContext db = new NothwindContext();
+    private bool ProductExists(int key)
+    {
+      return db.Products.Any(p => p.ProductID == key);
+    }
+
+    [EnableQuery]
+    public IQueryable<Product> Get()
+    {
+      return db.Products;
+    }
+
+    [EnableQuery]
+    public SingleResult<Product> Get([FromODataUri] int key)
+    {
+      IQueryable<Product> result = db.Products.Where(p => p.ProductID == key);
+      return SingleResult.Create(result);
+    }
+
+    public async Task<IHttpActionResult> Post(Product product)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+      db.Products.Add(product);
+      await db.SaveChangesAsync();
+      return Created(product);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      db.Dispose();
+      base.Dispose(disposing);
+    }
+  }
+}
